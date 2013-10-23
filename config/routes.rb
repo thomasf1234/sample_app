220528c611c3b59db795c2bd2ccebc8cb017c615
thomas.f <Thomas.Fisher@FootballPools.com>
2013-10-23 17:07:45 +0100

    updated comments in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,24 @@
 SampleApp::Application.routes.draw do
   get 'users/new'
-  root  'static_pages#home'
+  root  'static_pages#home' #changes url to '/' and is accessed by th root_path variable
  # get '/static_pages/home', to: 'static_pages#home', as: 'root'
   match '/signup', to: 'users#new', via: 'get'
 #  get '/static_pages/help', to: 'static_pages#help', as: 'help'
-  match '/help', to: 'static_pages#help', via: 'get'
+  match '/help', to: 'static_pages#help', via: 'get' #name roots, changes url to '/help' and is accessed by the help_path variable
   match '/about', to: 'static_pages#about', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
+
+=begin
+rake routes: #if this command is fed into the terminal, the routes set in the associated
+application directory are displayed. Below are the current routes in this application.
+   Prefix Verb URI Pattern          Controller#Action
+users_new GET /users/new(.:format) users#new
+     root GET /                    static_pages#home
+   signup GET /signup(.:format)    users#new
+     help GET /help(.:format)      static_pages#help
+    about GET /about(.:format)     static_pages#about
+  contact GET /contact(.:format)   static_pages#contact
+=end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
