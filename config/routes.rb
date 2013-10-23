b1ab7ca405aa97011fbe26f856634749481f9a10
thomas.f <Thomas.Fisher@FootballPools.com>
2013-10-23 17:00:16 +0100

    finished filling-in-layout, end of ch5

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/contact"
+  get 'users/new'
+  root  'static_pages#home'
+ # get '/static_pages/home', to: 'static_pages#home', as: 'root'
+  match '/signup', to: 'users#new', via: 'get'
+#  get '/static_pages/help', to: 'static_pages#help', as: 'help'
+  match '/help', to: 'static_pages#help', via: 'get'
+  match '/about', to: 'static_pages#about', via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
